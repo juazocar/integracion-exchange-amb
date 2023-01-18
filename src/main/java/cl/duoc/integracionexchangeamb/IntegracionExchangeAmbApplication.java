0a66346f3fdb111dc723974adaea14116812447d
jazocar <jazocar@gasco.cl>
2023-01-18 16:02:58 -0300

    first commit

---
+++ src/main/java/cl/duoc/integracionexchangeamb/IntegracionExchangeAmbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracionexchangeamb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionExchangeAmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionExchangeAmbApplication.class, args);
+	}
+
+}
